3b2fdbe6ae683ff84b39c8e890c8755ceebe98a6
Ishan Jain <contact@ishanjain.me>
2025-04-06 17:54:37 +0530

    fix: set value selected by dropdown

--- src/components/Settings/SettingsNetwork/index.tsx
+++ src/components/Settings/SettingsNetwork/index.tsx
@@ -325,6 +325,12 @@ const SettingsNetwork = () => {
                                 className="inline"
                                 id="forwardAuthUserHeader"
                                 name="forwardAuthUserHeader"
+                                onChange={(e) => {
+                                  setFieldValue(
+                                    'forwardAuthUserHeader',
+                                    e.target.value
+                                  );
+                                }}
                               >
                                 {ForwardAuthAllowlist.map((item) => (
                                   <option value={item} key={item}>
@@ -359,6 +365,12 @@ const SettingsNetwork = () => {
                                 className="inline"
                                 id="forwardAuthEmailHeader"
                                 name="forwardAuthEmailHeader"
+                                onChange={(e) => {
+                                  setFieldValue(
+                                    'forwardAuthEmailHeader',
+                                    e.target.value
+                                  );
+                                }}
                               >
                                 <option value="">--Do not use--</option>
                                 {ForwardAuthAllowlist.map((item) => (
